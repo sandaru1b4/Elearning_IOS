23c992379a2084d170f01c4bfe2653a094219ae7
Achitha Sandaruwan <sandaruwanb4@gmail.com>
2022-10-15 21:24:08 +0530

    Added FooterView / LessonsVC changes

--- Elearning_IOS/VC+VM/LessonsVC.swift
+++ Elearning_IOS/VC+VM/LessonsVC.swift
@@ -14,7 +14,6 @@ class LessonsVC: UIViewController {
     //MARK: - PROPERTIES
     //outlets
     @IBOutlet weak var coverVideoView: UIView!
-    @IBOutlet weak var bottomView: UIView!
     @IBOutlet weak var tableView: UITableView!
     
     //variables
@@ -27,19 +26,9 @@ class LessonsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
         startVideo()
         setupTableView()
-        setupTableViewHeader()
-        
     }
-    
-    
-    //setup ui
-    func setupUI() {
-        bottomView.addLayerEffects(with: UIColor(named: "boaderClr"), borderWidth: 1, cornerRadius: 30)
-    }
-    
     
     //strat cover video on view load
     func startVideo() {
@@ -62,35 +51,59 @@ class LessonsVC: UIViewController {
     }
     
     private func setupTableView() {
-
+        
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
-
+        setupTableViewHeader()
+        setupTableViewFooter()
+        
     }
     
     
+    //setup header
     private func setupTableViewHeader() {
-
+        
         let header = HeaderView(frame: .zero)
-
+        
         //set frame size before content load
         var size = header.systemLayoutSizeFitting(UIView.layoutFittingExpandedSize)
         size.width = UIScreen.main.bounds.width
         header.frame.size = size
-
+        
         //recalculate after content load
         size = header.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
         size.width = UIScreen.main.bounds.width
         header.frame.size = size
-
+        
         tableView.tableHeaderView = header
-
+        
+    }
+    
+    //setup footer
+    private func setupTableViewFooter() {
+        
+        let footer = FooterView(frame: .zero)
+        
+        //set frame size before content load
+        var size = footer.systemLayoutSizeFitting(UIView.layoutFittingExpandedSize)
+        size.width = UIScreen.main.bounds.width
+        footer.frame.size = size
+        
+        //recalculate after content load
+        size = footer.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        size.width = UIScreen.main.bounds.width
+        footer.frame.size = size
+        
+        tableView.tableFooterView = footer
+        
     }
     
 }
 
 
+
+//MARK: - table view
 extension LessonsVC:UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //
@@ -99,7 +112,6 @@ extension LessonsVC:UITableViewDelegate {
 
 
 extension LessonsVC:UITableViewDataSource {
-    
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 30.0
